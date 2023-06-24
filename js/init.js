95f70b5e5a7bb94991c2b3757c09641c86f1073b
finaldgrh <finaldgrh.gmail.com>
2023-06-23 18:45:14 -0600

    Subido

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyBGr1bsNWlaZKMbycY6FrljMsYkkpQa8iI",
+  authDomain: "preyectodgrh.firebaseapp.com",
+  projectId: "preyectodgrh",
+  storageBucket: "preyectodgrh.appspot.com",
+  messagingSenderId: "169408073077",
+  appId: "1:169408073077:web:bdac55fc5c1b0e9dae173c",
+  measurementId: "G-ZLCM79ZDZ7"
+});
